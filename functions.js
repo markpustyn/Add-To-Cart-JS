2088935e9eac54124120599f96e5f9c7cfb3b16e
markpustyn <108764244+markpustyn@users.noreply.github.com>
2023-05-17 11:37:20 -0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,14 @@
+let scrimbaUsers = {
+    "00": "sindre@scrimba.com",
+    "01": "per@scrimba.com",
+    "02": "frode@scrimba.com"
+}
+// Challenge: Create a let variable called 'scrimbaUsersEmails' and use one of Object methods to set it equal to an array with the values
+let scrimbaUsersEmails = Object.values(scrimbaUsers)
+// Challenge: Create a let variable called 'scrimbaUsersIDs' and use one of Object methods to set it equal to an array with the keys
+let scrimbaUsersIDs = Object.keys(scrimbaUsers)
+// Challenge: Create a let variable called 'scrimbaUsersEntries' and use one of Object methods to set it equal to an array with the both the keys and values
+let scrimbaUsersEntries = Object.assign(scrimbaUsers)
+console.log(scrimbaUsersEmails)
+console.log(scrimbaUsersIDs)
+console.log(scrimbaUsers)
